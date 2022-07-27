eb7ed0fc895485326b3cc06ba9e8ff3e476953e7
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-27 17:40:32 +0530

    Jenkins1

---
+++ src/test/java/Automate/Jenkins1/Compose.java
@@ -0,0 +1,72 @@
+package Automate.Jenkins1;
+
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Compose {
+	public WebDriver driver;
+	public WebDriverWait w; 
+	public String url= "https://mail.google.com/";
+	public Actions act;
+
+	@Test 
+	public void browser() {
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Automate/drivers/chromedriver");
+		ChromeOptions ch = new ChromeOptions();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	    ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--no-sandbox");		
+		driver=new ChromeDriver(ch);
+		driver.manage().window().maximize();
+	    driver.navigate().to(url);
+	}
+	
+	
+	@Test
+	public void composeemail() throws InterruptedException {
+		
+		Actions act = new Actions(driver);
+		driver.findElement(By.name("identifier")).sendKeys("abcdeSfuse@gmail.com");
+		act.sendKeys(Keys.ENTER).build().perform();		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		WebElement WelcomeMsg = driver.findElement(By.xpath("//span[contains(text(), 'Welcome')]"));
+		w = new WebDriverWait(driver, Duration.ofSeconds(30));
+		w.until(ExpectedConditions.visibilityOf(WelcomeMsg));
+		String message = WelcomeMsg.getText();
+		System.out.println(message);
+		Assert.assertEquals("Welcome", message);
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Sagar@123");
+		act.sendKeys(Keys.ENTER).build().perform();		
+		driver.findElement(By.xpath("//div[text()='Compose']")).click();
+		driver.findElement(By.className("vO")).click();
+		driver.findElement(By.className("vO")).sendKeys("abcdsourcefuse@gmail.com");
+		driver.findElement(By.className("aoT")).sendKeys("SourceFuse");
+		driver.findElement(By.className("Am")).sendKeys("This is a test email");
+		driver.findElement(By.xpath("//div[text()='Send']")).click();
+		String sentmessage = driver.findElement(By.xpath("//span[text()='Message sent']")).getText();
+		System.out.println(sentmessage);
+	}
+	
+ @Test
+ public void printText() {
+	 System.out.println("printing a demo sample text");
+ }
+	
+	
+ //@Test
+	public void close() {
+		driver.quit();
+	}
+	
+	
+}
---
+++ src/test/java/Automate/Jenkins1/Login.java
@@ -0,0 +1,79 @@
+package Automate.Jenkins1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Login {
+	
+public WebDriver driver;
+public WebDriverWait w; 
+public String url= "https://accounts.google.com/";
+public Actions act;
+
+
+
+@Test (priority=0)
+public void initialize() {
+
+	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Automate/drivers/chromedriver");
+	ChromeOptions ch = new ChromeOptions();
+    ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--no-sandbox");		
+	driver=new ChromeDriver(ch);
+	act = new Actions(driver);
+	driver.manage().window().maximize();
+	driver.get(url);
+
+}
+
+@Test (priority=1)
+public void blank() {
+	
+	driver.findElement(By.name("identifier")).sendKeys(" ");
+	act.sendKeys(Keys.ENTER).build().perform();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+	String text = driver.findElement(By.xpath("//*[text()='Enter an email or phone number']")).getText();
+	System.out.println(text);
+	Assert.assertEquals("Enter an email or phone number", text);
+	
+}
+
+
+@Test (priority=2)
+public void invalidemail() throws InterruptedException {
+	
+	driver.navigate().to(url);
+	driver.findElement(By.name("identifier")).sendKeys("sagar.tndu@gmail");
+	act.sendKeys(Keys.ENTER).build().perform();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+	String text = driver.findElement(By.xpath("//*[text()='Enter a valid email or phone number']")).getText();
+	System.out.println(text);
+	Assert.assertEquals("Enter a valid email or phone number", text);
+	
+}
+	
+
+@Test (priority=3)
+public void invalidphone() {
+	
+	driver.navigate().to(url);
+	driver.findElement(By.name("identifier")).sendKeys("9177@14370");
+	act.sendKeys(Keys.ENTER).build().perform();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	String text = driver.findElement(By.xpath("//*[text()='Enter a valid email or phone number']")).getText();
+	System.out.println(text);
+	Assert.assertEquals("Enter a valid email or phone number", text);
+	
+    }
+	
+}
